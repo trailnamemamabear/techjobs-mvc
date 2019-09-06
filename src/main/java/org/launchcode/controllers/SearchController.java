af62f23878b2fdb5a4a1f7198c1547b60bbed445
Holly Parker <hollyfparker@gmail.com>
2019-09-05 19:15:11 -0500

    commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,7 +22,7 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+
     @RequestMapping("results")
     public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model ) {
         ArrayList<HashMap<String, String>> jobs;
